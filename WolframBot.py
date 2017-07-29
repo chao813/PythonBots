565c2e99784330db1f6c3a6db4b516ebb65c9ab9
chao813 <chao9@purdue.edu>
2017-07-29 10:02:17 -0400

    firstcommit

---
+++ WolframBot.py
@@ -0,0 +1,30 @@
+import wolframalpha
+
+query = raw_input('Enter your math question: ')
+app_id='PA237Q-XA42375UEE'
+
+
+client = wolframalpha.Client(app_id)
+
+def wolfram(query):
+    res = client.query(str(query))
+    try:
+        ans=next(res.results).text
+    except:
+        ans ='I don\'t understand, please try again'
+    return ans
+
+print wolfram(query)
+
+q = 'yes'
+while q =='yes':
+    q = raw_input('Any other questions (yes/no)? ')
+    if q =='no':
+        print 'Thank you for using WolframAlpha Bot'
+        break
+    elif q =='yes':
+        query = raw_input('Enter your math question: ')
+        print wolfram(query)
+    else:
+        print 'I don\'t understand, please try again'
+        q ='yes'
