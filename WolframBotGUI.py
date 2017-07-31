fbe4c57fb6b562ac409e53980f5915b70f408115
chao813 <chao9@purdue.edu>
2017-07-31 13:41:59 -0400

    secondcommit (minor edits)

--- WolframBotGUI.py
+++ WolframBotGUI.py
@@ -19,6 +19,9 @@ def Call(*args):
     
     
     res = Label(root, text = meter).grid(column=2, row=2, sticky=W)
+    button['fg'] = 'white'
+    button['bg'] = 'blue'
+    
     #res.pack()
     
     
@@ -26,16 +29,20 @@ root = Tk()
 root.title("Math Solver")
 root.geometry('300x100+650+300')
 
+
 feet = StringVar()
 meters = StringVar()
 ttk.Label(text="Enter your question here:   ").grid(row=0, sticky=W)
 feet_entry = ttk.Entry(textvariable=feet)
 feet_entry.grid(column=2, row=0, sticky= W)
+ttk.Label(text="Your answer is:   ").grid(row=2, sticky=E)
 
 
-ttk.Button(text="Calculate", command=Call).grid(column=2, row=8, sticky=W)
+#Calculate button features uses Tkinter instead of tkk
+button = Button(text="Calculate", command=Call)
+button.grid(column=2, row=8, sticky=W)
 
-ttk.Label(text="Your Answer Is:   ").grid(row=2, sticky=E)
+
 
 
 
