027b30ecc7bf08c6dd76fd667c3f8c0d2ed4364c
chao813 <chao9@purdue.edu>
2017-08-01 14:19:52 -0400

    updated 8/1

--- WolframBotGUI.py
+++ WolframBotGUI.py
@@ -1,6 +1,8 @@
 from Tkinter import *
 import wolframalpha
 import ttk
+from PIL import ImageTk
+import base64, zlib
 
 app_id='PA237Q-XA42375UEE'
 client = wolframalpha.Client(app_id)
@@ -10,44 +12,66 @@ def Call(*args):
     #query = feet.get()
     #res = client.query(str(query))
     
+    
     try:
         query = feet.get()
         res = client.query(str(query))
-        meter = next(res.results).text
+        meters.set(next(res.results).text)
     except:
-        meter ='I don\'t understand, please try again'
+        meters.set('I don\'t understand, please try again')
     
     
-    res = Label(root, text = meter).grid(column=2, row=2, sticky=W)
-    button['fg'] = 'white'
-    button['bg'] = 'blue'
+    #res = Label(root, text = meters).grid(column=2, row=2, sticky=W)
     
-    #res.pack()
+    button['fg'] = 'black'
+    button['bg'] = 'light blue'
+    #button.after(2000, lambda: button.configure(background ="grey"))
+    return meters
+    
+#clear button to empty all values
+def clear_text(*args):
+    meters_entry.delete(0,'end')
+    feet_entry.delete(0, 'end')
     
     
 root = Tk()
 root.title("Math Solver")
-root.geometry('300x100+650+300')
+root.geometry('350x100+650+300')
+
+#Top right icon to transparent
+ICON = zlib.decompress(base64.b64decode('eJxjYGAEQgEBBiDJwZDBy'
+    'sAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
+image=ImageTk.PhotoImage(data=ICON)
+root.tk.call('wm', 'iconphoto', root._w, image)  
 
 
 feet = StringVar()
 meters = StringVar()
-ttk.Label(text="Enter your question here:   ").grid(row=0, sticky=W)
+ttk.Label(text="Enter your question here:   ", font = "Helvetica 10 bold").grid(row=0, sticky=W)
 feet_entry = ttk.Entry(textvariable=feet)
 feet_entry.grid(column=2, row=0, sticky= W)
-ttk.Label(text="Your answer is:   ").grid(row=2, sticky=E)
+ttk.Label(text="Your answer is:   ",font = "Helvetica 10 bold").grid(row=2, sticky=E)
+meters_entry = ttk.Entry(textvariable=meters)
+meters_entry.grid(column=2, row=2, sticky= W)
 
-
-#Calculate button features uses Tkinter instead of tkk
+#calculate button
 button = Button(text="Calculate", command=Call)
+button['bg'] ='light blue'
 button.grid(column=2, row=8, sticky=W)
 
-
+#clear button
+clear_button = Button(text = "Clear", command = clear_text)
+clear_button.grid(column = 0, row=8, sticky = E)
+clear_button['fg'] = 'black'
+clear_button['bg'] = 'light blue'
 
 
 
 feet_entry.focus()
 root.bind('<Return>', Call)
+
+for child in root.winfo_children():
+    child.grid_configure(padx=3, pady=3)
 
 
 root.mainloop() 
